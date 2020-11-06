a57bfc9a56cc2679e042cccf4458b9f8e7d1b7a7
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-11-06 15:35:38 -0800

    added foundation for app.js and artist.js

---
+++ Backend/routes/artist.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+
+
+router.get("/", async function(req, res, next) {
+  try {
+
+  }
+
+  catch (err) {
+    return next(err);
+  }
+});
+
+module.exports = router;
