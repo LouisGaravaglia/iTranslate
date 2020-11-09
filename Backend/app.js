8981ba77e1e76b9b7c8d8b3d5496a1f0925a999c
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-11-09 14:16:52 -0800

    receiving error from IBM while trying to make call to translate, sorting now

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,12 @@
+require('dotenv').config()
 const express = require("express");
 const app = express();
 // app.use(express.json());
 const translateRoutes = require("./routes/translate");
-const artistRoutes = require("./routes/artists");
 
 app.use("/translate", translateRoutes);
-app.use("/artists", artistRoutes);
+
+
 
 /** 404 handler */
 
--- Backend/routes/translate.js
+++ Backend/routes/translate.js
@@ -2,23 +2,42 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 const LanguageTranslatorV3 = require('ibm-watson/language-translator/v3');
 const { IamAuthenticator } = require('ibm-watson/auth');
-const {IBM_API_KEY} = 
+const IBM_API_KEY = process.env.IBM_API_KEY;
+const IBM_URL  = process.env.IBM_URL;
+// const IBM_VERSION = process.env.IBM_VERSION;
 
 const languageTranslator = new LanguageTranslatorV3({
-  version: '5.7.1',
+  version: '2018-05-01',
   authenticator: new IamAuthenticator({
-    apikey: '{apikey}',
+    apikey: IBM_API_KEY,
   }),
-  serviceUrl: '{url}',
+  serviceUrl: IBM_URL,
 });
+
+const translateParams = {
+  text: 'Hello friend',
+  modelId: 'en-es',
+};
+
+
 
 
 
 
 router.get("/", async function(req, res, next) {
-  const lyrics = req.body.translate
-  try {
 
+  try {
+    console.log("API KEY", IBM_API_KEY);
+    console.log("API URL", IBM_URL);
+    // console.log("IBM VERSION", IBM_VERSION);
+    languageTranslator.translate(translateParams)
+    .then(translationResult => {
+      console.log(JSON.stringify(translationResult, null, 2));
+      return JSON.stringify(translationResult, null, 2);
+    })
+    .catch(err => {
+      console.log('error:', err);
+    });
   }
 
   catch (err) {
