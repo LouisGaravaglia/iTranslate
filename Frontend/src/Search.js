cea897de2051265801f9375456ba3a8a886cf2fb
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-11-11 09:54:07 -0800

    Half of the useRef's are working on the search page to hide everything but search bar at first, then once a submission is made, DOM scrollIntoView for the searchResults. At the moment an empty 100% width displayLyrics div will appear as well

--- Frontend/src/Search.js
+++ Frontend/src/Search.js
@@ -7,7 +7,7 @@ import IBMWatsonAPI from "./IBMWatsonAPI";
 const Search = () => {
   const [searchVal, setSearchVal] = useState("")
   const [searchResults, setSearchResults] = useState([]);
-  const [lyrics, setLyrics] = useState([]);
+  const [lyrics, setLyrics] = useState("");
   const [translation, setTranslation] = useState("");
   const searchResultsRef = useRef();
   const lyricsTranslationRef = useRef();
@@ -60,7 +60,39 @@ const Search = () => {
       </div>
   )
 
-  return (
+  // return (
+  //   <div className="Search">
+  //     <div className="Search-Field">
+  //       <p></p>
+  //       <h1>Find your song!</h1>
+  //       <form onSubmit={handleSubmit}>
+  //       <div className="Search-Input-Container">
+  //           <input
+  //             type="text"
+  //             id="SearchVal"
+  //             name="searchVal"
+  //             value={searchVal}
+  //             onChange={handleChange}
+  //           />
+  //           <button type="submit"><i class="fa fa-search icon"></i></button>
+  //         </div>
+  //       </form>
+  //       <p></p>
+  //     </div>
+
+  //     <div className="Search-Results" ref={searchResultsRef}>
+  //       {searchResults[0] ? searchResults.map(r => <SearchResult getLyrics={getLyrics} artist={r.artists[0].name} album={r.album.name} track={r.name} trackId={r.id} artistId={r.artists[0].id} albumId={r.album.id}/>) : <p>Or select a random song!</p>}
+  //     </div>
+
+  //     {/* {searchResults[0] ? SearchResultsDiv : ""}
+  //     {lyrics !== "" ? LyricsTranslationDiv : ""} */}
+      
+  //     {LyricsTranslationDiv}
+      
+  //   </div>
+  // );
+
+    return (
     <div className="Search">
       <div className="Search-Field">
         <p></p>
@@ -80,17 +112,19 @@ const Search = () => {
         <p></p>
       </div>
 
-      <div className="Search-Results" ref={searchResultsRef}>
-        {searchResults[0] ? searchResults.map(r => <SearchResult getLyrics={getLyrics} artist={r.artists[0].name} album={r.album.name} track={r.name} trackId={r.id} artistId={r.artists[0].id} albumId={r.album.id}/>) : <p>Or select a random song!</p>}
+   
+
+      {searchResults[0] ? SearchResultsDiv : ""}
+
+   
+      <div className={searchResults[0] ? "Browse-Lyrics-Translation" : ""} ref={lyricsTranslationRef}>
+        {lyrics !== "" ? LyricsTranslationDiv : ""}
       </div>
 
-      {/* {searchResults[0] ? SearchResultsDiv : ""}
-      {lyrics !== "" ? LyricsTranslationDiv : ""} */}
-      
-      {LyricsTranslationDiv}
       
     </div>
   );
+
 };
 
 export default Search;
