d09a55bd13198f58a4b7cc9190935b9a4a033037
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-11-11 13:49:57 -0800

    set up my db.js file

---
+++ Backend/db.js
@@ -0,0 +1,17 @@
+const { client } = require("pg");
+
+let DB_URI;
+
+if ( process.env.NODE_ENV === "test" ) {
+  DB_URI = "postgresql:///itranslatedb_test";
+} else {
+  DB_URI = "postgresql:///itranslatedb";
+}
+
+let db = new Client( {
+  connectionString: DB_URI;
+} )
+
+db.connect();
+
+module.exports = db;
