b557d965bab3fb31da3819125fcd051ac9c1452b
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-11-15 12:15:29 -0800

    slider is working, but really only a good UX if the array.length is 20. going to work on dynamically updaing the pivot value to display based on array length

--- Frontend/src/SearchResultList.js
+++ Frontend/src/SearchResultList.js
@@ -3,20 +3,46 @@ import Slider from '@material-ui/core/Slider';
 import SearchResult from "./SearchResult";
 
 const SearchResultList = ({resultsArray, handleSearch}) => {
-  const [value, setValue] = useState();
+  const [sliderVal, setSliderVal] = useState(0);
   const [paginateVal, setPaginateVal] = useState(0);
 
   const resultsInView = resultsArray.slice(paginateVal, paginateVal + 5);
 
+  const pivotVal = (100 / resultsArray.length) * 5;
+
   const handleChange = (event, newValue) => {
-    setValue(newValue);
+    setSliderVal(newValue);
+    if (sliderVal >= 0 && sliderVal <= 25 && resultsArray.length > 0 ) {
+      setPaginateVal(0);
+    } else if (sliderVal > 25 && sliderVal <= 50 && resultsArray.length > 5) {
+      setPaginateVal(5);
+    } else if (sliderVal > 50 && sliderVal <= 75 && resultsArray.length > 10 ) {
+      setPaginateVal(10);
+    } else if (sliderVal > 75 && sliderVal <= 100 && resultsArray.length > 15 ) {
+      setPaginateVal(15);
+    } 
+    console.log("Slider Value: ", sliderVal);
   };
+
+  // const handleChange = (event, newValue) => {
+  //   setSliderVal(newValue);
+  //   if (sliderVal >= 0 && sliderVal <= 25 && resultsArray.length > 0 ) {
+  //     setPaginateVal(0);
+  //   } else if (sliderVal > 25 && sliderVal <= 50 && resultsArray.length > 5) {
+  //     setPaginateVal(5);
+  //   } else if (sliderVal > 50 && sliderVal <= 75 && resultsArray.length > 10 ) {
+  //     setPaginateVal(10);
+  //   } else if (sliderVal > 75 && sliderVal <= 100 && resultsArray.length > 15 ) {
+  //     setPaginateVal(15);
+  //   } 
+  //   console.log("Slider Value: ", sliderVal);
+  // };
 
 
   return (
     <>
       {resultsInView.map((r, i) => <SearchResult key={i} index={i} getLyrics={handleSearch} artist={r.artists[0].name} album={r.album.name} track={r.name} trackId={r.id} artistId={r.artists[0].id} albumId={r.album.id}/>)}
-      <Slider className="Search-Slider" value={value} onChange={handleChange} aria-labelledby="continuous-slider" />
+      {resultsArray.length > 5 && <Slider className="Search-Slider" color="secondary" value={sliderVal} onChange={handleChange} aria-labelledby="continuous-slider" />}
     </>
   );
 };
