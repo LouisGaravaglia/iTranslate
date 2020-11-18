7ca40b9d648fd373839b8c0414e2aa42b2d23bc6
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-11-17 19:55:40 -0800

    refactored the Browse Component to get rid of any unused variables and unused pieces of state

--- Frontend/src/Browse.js
+++ Frontend/src/Browse.js
@@ -1,51 +1,29 @@
 import React,  {useState, useRef, useEffect, useCallback} from 'react';
 import './App.css';
-//API IMPORTS
-import SpotifyAPI from "./SpotifyAPI";
 //COMPONENT IMPORTS
-import DisplayLyrics from "./DisplayLyrics";
-import SearchBar from "./SearchBar";
 import FlashMessage from "./FlashMessage";
-import SearchResultList from "./SearchResultList";
 import BrowseByArtists from "./BrowseByArtists";
 //REDUX IMPORTS
 import {useDispatch, useSelector} from "react-redux";
-import {getTranslation} from "./actionCreators/getTranslationCreator";
 import {resetLanguageError, resetTranslationError, resetLyricsError} from "./actionCreators/handleErrorsCreator";
-import {getLyrics} from "./actionCreators/getLyricsCreator";
-import {getAlbums} from "./actionCreators/BrowseRoute/Artists/getAlbumsCreator";
-import {getTracks} from "./actionCreators/BrowseRoute/Artists/getTracksCreator";
 
 function Browse() {
-  const [selectedArtistId, setSelectedArtistId] = useState("");
+  //REACT STATE
   const [category, setCategory] = useState("");
-  const [selectedTrackId, setSelectedTrackId] = useState([]);
-  const [selectedAlbumId, setSelectedAlbumId] = useState([]);
-  const [completeAlbumData, setCompleteAlbumData] = useState({});
   //REDUX STORE
   const dispatch = useDispatch();
-  const languages = useSelector(store => store.languages);
-  const translation = useSelector(store => store.translation);
   const languageError = useSelector(store => store.errors.languageError);
   const translationError = useSelector(store => store.errors.translationError);
   const lyricsError = useSelector(store => store.errors.lyricsError);
-  const lyrics = useSelector(store => store.lyrics);
-  const artists = useSelector(store => store.artists);
-  const albums = useSelector(store => store.albums);
-  const tracks = useSelector(store => store.tracks);
   //STATE FOR FLASH MESSAGES
   const [searchFlashMessage, setSearchFlashMessage] = useState(false);
   const [noLyricsFlashMessage, setNoLyricsFlashMessage] = useState(false);
   const [languageNotFoundFlashMessage, setLanguageNotFoundFlashMessage] = useState(false);
   const [translationErrorFlashMessage, setTranslationErrorFlashMessage] = useState(false);
   //REFS FOR PAGE TRAVERSAL
-  const lyricsTranslationRef = useRef();
   const artistResultsRef = useRef();
   const genreResultsRef = useRef();
   const danceabilityDivRef = useRef();
-  const albumResultsRef = useRef();
-  const selectLanguageRef = useRef();
-  const trackResultsRef = useRef();
 
 ////////////////////////////////////////////////////  USE EFFECTS  ////////////////////////////////////////////////////
 
@@ -106,7 +84,6 @@ function Browse() {
         <BrowseByArtists />
       </div>
   );
-  
 
 ////////////////////////////////////////////////////  RETURN  ////////////////////////////////////////////////////
 
