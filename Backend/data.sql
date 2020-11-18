1c0547209c72f05f102a1fcf5308df9be4fb0b72
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-11-18 09:42:13 -0800

    updatd data.sql seed file for the new schema

--- Backend/data.sql
+++ Backend/data.sql
@@ -7,7 +7,8 @@ CREATE DATABASE itranslatedb;
 DROP TABLE IF EXISTS tracks;
 
 CREATE TABLE tracks(
-    spotify_id TEXT NOT NULL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
+    spotify_id TEXT NOT NULL,
     name TEXT NOT NULL,
     explicit BOOLEAN,
     popularity INT,
@@ -16,16 +17,20 @@ CREATE TABLE tracks(
     danceability FLOAT,
     tempo FLOAT,
     valence FLOAT,
-    duration INT 
+    duration INT,
+    lyrics TEXT,
+    album_id INT NOT NULL REFERENCES albums ON DELETE CASCADE,
+    artist_id INT NOT NULL REFERENCES artists ON DELETE CASCADE,
 );
 
 DROP TABLE IF EXISTS artists;
 
 CREATE TABLE artists(
-    spotify_id TEXT NOT NULL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
+    spotify_id TEXT NOT NULL,
     name TEXT NOT NULL,
-    genre TEXT NOT NULL,
-    spotify_uri TEXT NOT NULL,
+    genre TEXT,
+    spotify_uri TEXT,
     img_url TEXT,
     popularity INT
 );
@@ -33,29 +38,16 @@ CREATE TABLE artists(
 DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums(
-    spotify_id TEXT NOT NULL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
+    spotify_id TEXT NOT NULL,
     name TEXT NOT NULL,
     release_date TEXT,
-    spotify_uri TEXT NOT NULL,
-    img_url TEXT NOT NULL
+    spotify_uri TEXT,
+    img_url TEXT NOT NULL,
+    artist_id INT NOT NULL REFERENCES artists ON DELETE CASCADE,
 );
 
-DROP TABLE IF EXISTS discography;
 
-CREATE TABLE discography (
-  id SERIAL PRIMARY KEY,
-  track_id TEXT NOT NULL REFERENCES tracks ON DELETE CASCADE,
-  album_id TEXT NOT NULL REFERENCES albums ON DELETE CASCADE,
-  artist_id TEXT NOT NULL REFERENCES artists ON DELETE CASCADE
-);
-
-DROP TABLE IF EXISTS lyrics;
-
-CREATE TABLE lyrics (
-  id SERIAL PRIMARY KEY,
-  track_id TEXT NOT NULL REFERENCES tracks ON DELETE CASCADE,
-  lyrics TEXT NOT NULL
-);
 
 DROP TABLE IF EXISTS translation;
 
