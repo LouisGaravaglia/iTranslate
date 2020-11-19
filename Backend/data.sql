5723ef54fd1c394f9670cb1e85107b8738f43c5a
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-11-18 22:28:25 -0800

    Dropped the database and updated the schema

--- Backend/data.sql
+++ Backend/data.sql
@@ -4,30 +4,11 @@ CREATE DATABASE itranslatedb;
 
 \c itranslatedb;
 
-DROP TABLE IF EXISTS tracks;
-
-CREATE TABLE tracks(
-    id SERIAL PRIMARY KEY,
-    spotify_id TEXT UNIQUE NOT NULL,
-    name TEXT NOT NULL,
-    explicit BOOLEAN,
-    popularity INT,
-    preview_url TEXT,
-    spotify_uri TEXT NOT NULL,
-    danceability FLOAT,
-    tempo FLOAT,
-    valence FLOAT,
-    duration INT,
-    lyrics TEXT,
-    album_id INT NOT NULL REFERENCES albums ON DELETE CASCADE,
-    artist_id INT NOT NULL REFERENCES artists ON DELETE CASCADE,
-);
-
 DROP TABLE IF EXISTS artists;
 
 CREATE TABLE artists(
     id SERIAL PRIMARY KEY,
-    spotify_id TEXT UNIQUE NOT NULL UNIQUE,
+    spotify_id TEXT UNIQUE NOT NULL,
     name TEXT NOT NULL,
     genre TEXT,
     spotify_uri TEXT,
@@ -44,17 +25,34 @@ CREATE TABLE albums(
     release_date TEXT,
     spotify_uri TEXT,
     img_url TEXT NOT NULL,
-    artist_id INT NOT NULL REFERENCES artists ON DELETE CASCADE,
+    artist_id INT NOT NULL REFERENCES artists ON DELETE CASCADE
 );
 
+DROP TABLE IF EXISTS tracks;
 
+CREATE TABLE tracks(
+    id SERIAL PRIMARY KEY,
+    spotify_id TEXT UNIQUE NOT NULL,
+    name TEXT NOT NULL,
+    explicit BOOLEAN,
+    popularity INT,
+    preview_url TEXT,
+    spotify_uri TEXT NOT NULL,
+    danceability FLOAT,
+    tempo FLOAT,
+    valence FLOAT,
+    duration INT,
+    lyrics TEXT,
+    album_id TEXT NOT NULL REFERENCES albums(spotify_id) ON DELETE CASCADE,
+    artist_id TEXT NOT NULL REFERENCES artists(spotify_id )ON DELETE CASCADE
+);
 
 DROP TABLE IF EXISTS translation;
 
 CREATE TABLE translation (
   id SERIAL PRIMARY KEY,
-  track_id TEXT NOT NULL REFERENCES tracks ON DELETE CASCADE,
+  track_id TEXT NOT NULL REFERENCES tracks(spotify_id) ON DELETE CASCADE,
   language TEXT NOT NULL,
   translation TEXT NOT NULL
-);
+)
 
