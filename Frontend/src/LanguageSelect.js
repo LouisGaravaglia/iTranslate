6f3b696ddb0514f555735f82e134b7b71c23f542
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-11-22 12:38:00 -0800

    refactored the LanguageSelect and the LyricsTranslation components. Search route is working well. Moving onto Browse to import the new components

--- Frontend/src/LanguageSelect.js
+++ Frontend/src/LanguageSelect.js
@@ -1,8 +1,7 @@
-import React,  {useRef, useEffect, useCallback} from 'react';
+import React from 'react';
 import { Spring } from 'react-spring/renderprops';
 import './App.css';
 //COMPONENT IMPORTS
-import DisplayLyrics from "./DisplayLyrics";
 import SearchBar from "./SearchBar";
 //REDUX IMPORTS
 import {useDispatch, useSelector} from "react-redux";
@@ -12,28 +11,7 @@ function LanguageSelect({selectedTrackId}) {
   //REDUX STORE
   const dispatch = useDispatch();
   const languages = useSelector(store => store.languages);
-  const translation = useSelector(store => store.translation);
   const lyrics = useSelector(store => store.lyrics);
-  //REFS FOR PAGE TRAVERSAL
-  const lyricsTranslationRef = useRef();
-  const selectLanguageRef = useRef();
-
-////////////////////////////////////////////////////  USE EFFECTS  ////////////////////////////////////////////////////
-
-  // //FUNCTION TO BE CALLED IN BELOW USE-EFFECTS TO SCROLL TO NEXT DIV AFTER CLICK
-  // const scrollToNextDiv = useCallback(async (state, ref) => {
-
-  //   if (state && state !== "Could not read language value") {
-  //     ref.current.scrollIntoView({behavior: "smooth"});
-  //   }
-
-  // }, []);
-
-  // //SCROLL DOWN TO LANGUAGE SEARCH BAR WHEN SELECTED TRACK HAS BE SET IN STATE
-  // useEffect(() => {scrollToNextDiv(lyrics, selectLanguageRef);}, [lyrics, selectLanguageRef, scrollToNextDiv]);
-
-  // //SCROLL DOWN TO LYRICS/TRANSLATION WHEN LANGUAGE HAS BEEN SELECTED AND SET IN STATE
-  // useEffect(() => {scrollToNextDiv(translation, lyricsTranslationRef);}, [translation, lyricsTranslationRef, scrollToNextDiv]);
 
 ////////////////////////////////////////////////////  HANDLE CLICK FUNCTIONS  ////////////////////////////////////////////////////
 
@@ -45,23 +23,23 @@ function LanguageSelect({selectedTrackId}) {
 
   return (
     <>
-        <Spring
-    from={{opacity: 0}}
-    to={{opacity: 1}}
-    config={{delay: 300, duration: 300}}
-    >
+      <Spring
+        from={{opacity: 0}}
+        to={{opacity: 1}}
+        config={{delay: 300, duration: 300}}
+      >
+
       {props => (
         <div style={props}>
-      <div ref={selectLanguageRef}>
-        <SearchBar header="Select which language you'd like your lyrics translated to!" handleSubmit={handleLanguageSearchSubmit}/>
-      </div>
-      
-              </div>
+          <div>
+            <SearchBar header="Select which language you'd like your lyrics translated to!" handleSubmit={handleLanguageSearchSubmit}/>
+          </div>
+        </div>
       )}
+
     </Spring>
-    
   </>
   );
-}
+};
 
 export default LanguageSelect;
--- Frontend/src/LyricsTranslation.js
+++ Frontend/src/LyricsTranslation.js
@@ -1,36 +1,27 @@
-import React,  {useRef, useEffect, useCallback} from 'react';
+import React from 'react';
 import { Spring } from 'react-spring/renderprops';
 import './App.css';
-//COMPONENT IMPORTS
-import DisplayLyrics from "./DisplayLyrics";
-import SearchBar from "./SearchBar";
 //REDUX IMPORTS
-import {useDispatch, useSelector} from "react-redux";
-import {getTranslation} from "./actionCreators/getTranslationCreator";
+import {useSelector} from "react-redux";
 
-function LyricsTranslation({selectedTrackId}) {
+function LyricsTranslation() {
   //REDUX STORE
-  const dispatch = useDispatch();
-  const languages = useSelector(store => store.languages);
   const translation = useSelector(store => store.translation);
   const lyrics = useSelector(store => store.lyrics);
-  //REFS FOR PAGE TRAVERSAL
-  const lyricsTranslationRef = useRef();
-  const selectLanguageRef = useRef();
-
 
 ////////////////////////////////////////////////////  RETURN  ////////////////////////////////////////////////////
 
   return (
     <>
-        <Spring
-    from={{opacity: 0}}
-    to={{opacity: 1}}
-    config={{delay: 300, duration: 300}}
-    >
+      <Spring
+        from={{opacity: 0}}
+        to={{opacity: 1}}
+        config={{delay: 300, duration: 300}}
+      >
+
       {props => (
         <div style={props}>
-          <div className="Lyrics-Translation" ref={lyricsTranslationRef}>
+          <div className="Lyrics-Translation">
             <div className="Lyrics-Container">
               <p className="Lyrics">ORIGINAL LYRICS</p>
               <p className="Lyrics">{lyrics}</p>
@@ -40,12 +31,12 @@ function LyricsTranslation({selectedTrackId}) {
               <p className="Translation">{translation}</p>
             </div>
           </div>
-              </div>
+        </div>
       )}
+
     </Spring>
-    
   </>
   );
-}
+};
 
 export default LyricsTranslation;
