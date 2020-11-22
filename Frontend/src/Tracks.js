dba40fe17b2c2c83ba9a94d06e4b52be10f06521
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-11-22 14:58:47 -0800

    Tracks is now animating. Might refacotred Tracks component into somethign more simple

--- Frontend/src/Tracks.js
+++ Frontend/src/Tracks.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Spring} from 'react-spring/renderprops';
 //API IMPORTS
 import SpotifyAPI from "./SpotifyAPI";
 import BackendCall from "./BackendCall";
@@ -44,8 +45,21 @@ const Tracks = ({typeOfResults, results, itemsPerPage}) => {
 ////////////////////////////////////////////////////  RETURN  ////////////////////////////////////////////////////
 
   return (
-    <SearchResultList key={results[0].trackId} typeOfResults={typeOfResults} resultsArray={results} handleSearch={handleTrackResultsClick} itemsPerPage={itemsPerPage}/>
+    <Spring
+      from={{opacity: 0}}
+      to={{opacity: 1}}
+      config={{delay: 300, duration: 300}}
+    >
+      {props => (
+        <div style={props}>
+
+          <SearchResultList key={results[0].trackId} typeOfResults={typeOfResults} resultsArray={results} handleSearch={handleTrackResultsClick} itemsPerPage={itemsPerPage}/>
+
+        </div>
+      )}
+    </Spring>
   );
 };
+
 
 export default Tracks;
