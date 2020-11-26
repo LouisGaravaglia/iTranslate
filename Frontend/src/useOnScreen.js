72e09551f8df28136bf666673609ae333efac5c3
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-11-26 11:26:56 -0800

    ready to deploy frontend

--- Frontend/src/useOnScreen.js
+++ Frontend/src/useOnScreen.js
@@ -1,26 +1,33 @@
 import {useState, useEffect} from "react";
 
-  function useOnScreen(ref, options={threshold: 0.3}) {
-    // STATE AND SETTER FOR STORING WHETER ELEMENT IS VISIBLE
-    const [isIntersecting, setIntersecting] = useState(false);
+  /**
+* Hook that utilizes an IntersectionObserver to determine whether or not
+* a component is in the viewport. This will be used to set the appropriate 
+* background color.
+* @param {useRef} ref - input value form the search field
+* @param {object} options - input value form the search field
+*/
+function useOnScreen(ref, options={threshold: 0.3}) {
+  // STATE AND SETTER FOR STORING WHETER ELEMENT IS VISIBLE
+  const [isIntersecting, setIntersecting] = useState(false);
 
-    useEffect(() => {
-      const observer = new IntersectionObserver(([entry]) => {
-        // UPDATE OUR STATE WHEN OBSERVER CALLBACK FIRES
-        setIntersecting(entry.isIntersecting);
-      }, options);
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      // UPDATE OUR STATE WHEN OBSERVER CALLBACK FIRES
+      setIntersecting(entry.isIntersecting);
+    }, options);
 
+    if (ref.current) {
+      observer.observe(ref.current);
+    };
+
+    return () => {
       if (ref.current) {
-        observer.observe(ref.current);
+        observer.unobserve(ref.current);
       };
-
-      return () => {
-        if (ref.current) {
-          observer.unobserve(ref.current);
-        };
-      };
-    }, [ref, options]); 
-    return isIntersecting;
-  };
+    };
+  }, [ref, options]); 
+  return isIntersecting;
+};
 
 export default useOnScreen;
