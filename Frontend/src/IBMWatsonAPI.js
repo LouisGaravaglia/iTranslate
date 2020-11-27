398802abcd59cf6f022c22c754924ca59e0e8e83
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-11-26 20:57:41 -0800

    fixed url error i had in my ibm api component, shoudlnt have cors issues anymore

--- Frontend/src/IBMWatsonAPI.js
+++ Frontend/src/IBMWatsonAPI.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+
 class IBMWatsonAPI {
 
   static async getTranslationFromAPI(lyrics, language) {
     try {
       const res = await axios({
           method: "get",
-          url: `http://localhost:3001/ibm/translate`,
+          url: `${BASE_URL}/ibm/translate`,
           params: {lyrics, language},
       });
       const responseObj = JSON.parse(res.data);
@@ -20,7 +22,7 @@ class IBMWatsonAPI {
     try {
       const res = await axios({
           method: "get",
-          url: `http://localhost:3001/ibm/languages`
+          url: `${BASE_URL}/ibm/languages`
       });
       const responseObj = JSON.parse(res.data);
       return responseObj.response.result.languages;
