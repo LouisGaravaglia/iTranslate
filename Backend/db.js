a44c7bd204f542579000d4e6b1bdbd376711b8f6
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-11-26 21:54:40 -0800

    trying to hard code herokus postgres url into my db.config file

--- Backend/db.js
+++ Backend/db.js
@@ -5,7 +5,8 @@ let DB_URI;
 if ( process.env.NODE_ENV === "test" ) {
   DB_URI = "postgresql:///itranslatedb_test";
 } else {
-  DB_URI = "postgresql:///itranslatedb";
+  DB_URI = "postgres://xekuremvkzulik:6b8622ec721d9971708eb25b0630a7ef05bb07c46b41e1caa2522ef673a8f97e@ec2-34-200-106-49.compute-1.amazonaws.com:5432/d344hlhldv28n1";
+  // DB_URI = "postgresql:///itranslatedb";
 };
 
 let db = new Client({
