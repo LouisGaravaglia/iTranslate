be8ba14d11196c641458e2107847739f5347e593
Louis Garavaglia <louis.garavaglia@gmail.com>
2021-04-02 16:53:15 -0700

    site is working locally with updated npm packages

--- Backend/db.js
+++ Backend/db.js
@@ -2,7 +2,10 @@ const {Client} = require("pg");
 const {DB_URI} = require("./config");
 
 const db = new Client({
-  connectionString: DB_URI
+  connectionString: DB_URI,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect();
